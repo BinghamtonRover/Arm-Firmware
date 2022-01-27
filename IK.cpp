f341534148e89cbec9359357abade225f085d424
Levi Lesches <levilesches@gmail.com>
2022-01-26 20:44:37 -0500

    Completely overhauled IK.cpp and IK.h to use triangulation

--- IK.cpp
+++ IK.cpp
@@ -1,67 +1,65 @@
+#include <stdio.h>
+#include <tuple>
+
 #include "IK.h"
 
-//Check rotation validity first
+#define PI 3.141592653589793
 
-double IK::getYPrime(double x, double y) {
-	return sqrt(x * x + y * y);
+std::tuple<double, double> IK::calculatePosition(double B, double C) {
+	/* Computes the current position of the end-effector (hand).
+	
+	 We can do this using basic vector math: Treat all the arms as vectors and 
+	 add them together to get the position of the end effector.
+ */
+	
+	double absoluteC = B - PI + C;  // gets angle C relative to the X-axis
+	double x1 = a * cos(B);
+	double y1 = a * sin(B);
+	double x2 = b * cos(absoluteC);
+	double y2 = b * sin(absoluteC);
+	return {x1 + x2, y1 + y2};
 }
 
-void IK::failure() {
-	x = oldX;
-	y = oldY;
-	z = oldZ;
+std::tuple<double, double> IK::calculateAngles(double x, double y) {
+	/* Computes the values for angles B and C to move the gripper to (x, y).
+
+	 This function uses the Triangulation algorithm. Essentially, we use the Law 
+	 of Cosines combined with the lengths of the arms to compute the angles of the
+	 joints, working backwards from the (x, y) destination. 
+
+	 Returns a failure (angles of -1) if the math doesn't check out ([tolerance]),
+	 or the computed angles are out-of-bounds for the rover (like [bLimits]).
+
+	 For more details, see:
+	 - the diagram labeled `diagram.png` in this repository
+	 - the interactive demo at https://www.desmos.com/calculator/i8grld5pdu
+	 - this paper from the inventors of the algorithm: 
+	   https://www.researchgate.net/publication/251743615_Triangulation_A_new_algorithm_for_Inverse_Kinematics
+	*/
+
+	// Use the law of cosines to determine the two angles.
+	double c = sqrt(y*y + x*x);
+	double C  = acos( (a*a + b*b - c*c) / (2*a*b) );
+	double B1 = acos( (a*a - b*b + c*c) / (2*a*c) );
+	double B2 = atan(y / x);
+	double B  = B1 + B2;
+
+	// Double-check the logic by solving for the supposed position.
+	auto [computedX, computedY] = calculatePosition(B, C);
+	double deltaX = abs(computedX - x);
+	double deltaY = abs(computedY - y);
+	if (deltaX > tolerance || deltaY > tolerance) return {-1, -1};
+
+	// Check for forbidden angles
+	if (B < bLimits[0] || B > bLimits[1]) return {-1, -1};
+	else if (C < cLimits[0] || C > cLimits[1]) return {-1, -1};
+
+	return {B, C};
 }
 
-void IK::chooseOption() {
-	if (x >= j1Limits[0] && x <= j1Limits[1]) {
-		if (((fabs(y - o1Angles[0])) + (fabs(z - o1Angles[1]))) <= (((fabs(y - o2Angles[0])) + (fabs(z - o2Angles[1]))))) {
-			if (o1Angles[0] >= j2Limits[0] && o1Angles[0] <= j2Limits[1] && o1Angles[1] >= j3Limits[0] && o1Angles[1] <= j3Limits[1]) {
-				y = o1Angles[0];
-				z = o1Angles[1];
-			}
-			else if (o2Angles[0] >= j2Limits[0] && o2Angles[0] <= j2Limits[1] && o2Angles[1] >= j3Limits[0] && o2Angles[1] <= j3Limits[1]) {
-				y = o2Angles[0];
-				z = o2Angles[1];
-			}
-			else failure();
-		}
-
-		else {
-			if (o2Angles[0] >= j2Limits[0] && o2Angles[0] <= j2Limits[1] && o2Angles[1] >= j3Limits[0] && o2Angles[1] <= j3Limits[1]) {
-				y = o2Angles[0];
-				z = o2Angles[1];
-			}
-			else failure();
-		}
-	}
-	else failure();
-}
-
-void IK::doubleCheck() {
-	// TODO: These values aren't used!
-	double finalZ1 = sin(o1Angles[0]) * L2 + sin(o1Angles[1]) * L3;
-	double finalZ2 = sin(o2Angles[0]) * L2 + sin(o2Angles[1]) * L3;
-
-	double finalY1 = sin(x) * (cos(o1Angles[0]) * L2 + cos(o1Angles[1]) * L3);
-	double finalY2 = sin(x) * (cos(o2Angles[0]) * L2 + cos(o2Angles[1]) * L3);
-
-	double finalX1 = cos(x) * (cos(o1Angles[0]) * L2 + cos(o1Angles[1]) * L3);
-	double finalX2 = cos(x) * (cos(o2Angles[0]) * L2 + cos(o2Angles[1]) * L3);
-}
-
-void IK::getAngles(double newX, double newY, double newZ) {
-	oldX = x;
-	oldY = y;
-	oldZ = z;
-	x = atan2(newY, newX);
-
-	chooseOption();
-	doubleCheck();
-}
-
-void IK::endPointPos() {
-	// TODO: These values aren't used!
-	double endPointZ = sin(y) * L2 + sin(z) * L3;
-	double endPointY = sin(x) * (cos(y) * L2 + cos(z) * L3);
-	double endPointX = cos(x) * (cos(y) * L2 + cos(z) * L3);
+int main() { 
+	auto [shoulder, elbow] = IK::calculateAngles(9.33, 6.16);
+	double shoulderDegrees = shoulder * 180/PI;
+	double elbowDegrees = elbow * 180/PI;
+	printf("Shoulder: %f, Elbow: %f", shoulderDegrees, elbowDegrees);
 }
--- IK.ino
+++ IK.ino
@@ -1,3 +1,20 @@
+/* Computes and saves the angles required to move to the given position 
+
+ Since 3D is confusing, let's define our axes from the operator's perspective:
+ - The X-axis: left(-) and right(+)
+ - The Y-axis: down(-) and up(+)
+ - The Z-axis: backward(-) and forward(+)
+
+ This function sets [shoulderAngle], [elbowAngle], and [wristAngle] such
+ that the end effector -- the hand -- is at the given position. Since the
+ controls work in two dimensions at a time, we have two ways of moving the arm: 
+   1. forward and backwards, such that the hand is only moving in the Z-axis.
+   2. horizontal and vertical, such that the Z-position remains constant.
+
+  For motion 1 we need to use IK, but for motion 2 it would suffice to simply
+  rotate the base at the shoulder (ie, swivel the arm).
+*/
+
 void setCoordinates() {
   switch(mainMovement) {
     case forward:
