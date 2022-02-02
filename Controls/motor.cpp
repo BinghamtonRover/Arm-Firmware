89526b17ab59751c353ffefecb588cffc1fef594
Levi Lesches <levilesches@gmail.com>
2022-02-02 02:01:28 -0500

    Added a new Motor class to abstract motor logic

---
+++ Controls/motor.cpp
@@ -0,0 +1,4 @@
+#include "motor.h"
+
+void Motor::init() { }
+void Motor::write(double angle) { }
---
+++ Controls/motor.h
@@ -0,0 +1,10 @@
+#ifndef motor_h
+#define motor_h
+
+class Motor { 
+	public: 
+		void init();
+		void write(double angle);	
+};
+
+#endif
