f558d536f9bfb6299fcb8cbb801f57383e267d9c
Seth Brokalis <71616577+sbrokal1@users.noreply.github.com>
2022-09-02 10:26:02 -0400

    Uncomment CAN handlers and add update to Gripper

--- Arm/Arm.ino
+++ Arm/Arm.ino
@@ -3,7 +3,7 @@
 #include <BURT_arm_motor.h>
 #include <BURT_can.h>
 
-/* TODO: 
+/* TODO:
  - handle gyroscope inputs
  - handle recoil
 */
@@ -22,13 +22,13 @@
 #define LIFT_LS_PIN 9
 
 // Limits for the joints, in terms of radians from their limit switches
-#define SWIVEL_MIN -2 * PI  // INFINITY // can swivel in endless circles
+#define SWIVEL_MIN -2 * PI // INFINITY // can swivel in endless circles
 #define SWIVEL_MAX 2 * PI
 #define SWIVEL_GEARBOX 46.656
 
 #define EXTEND_MIN 2.00 // -0.698
 #define EXTEND_MAX 2.18
-#define EXTEND_GEARBOX -331.816  
+#define EXTEND_GEARBOX -331.816
 
 #define LIFT_MIN 0.698
 #define LIFT_MAX 2.443
@@ -49,20 +49,21 @@
 #define CAN_CALIBRATE_ID 0
 
 // Misc
-#define MOTOR_CURRENT 3000  // current ratings for stepper motors
+#define MOTOR_CURRENT 3000 // current ratings for stepper motors
 
-/* The (x, y, z) coordinates of the gripper. 
+/* The (x, y, z) coordinates of the gripper.
 
- These coordinates are passed to the IK algorithms which return angles for the 
- joints of the arm, [swivel], [extend], and [lift]. 
-*/ 
+ These coordinates are passed to the IK algorithms which return angles for the
+ joints of the arm, [swivel], [extend], and [lift].
+*/
 double gripperX = 0, gripperY = 0, gripperZ = 0;
 
 StepperMotor swivel = StepperMotor(SWIVEL_CS_PIN, SWIVEL_EN_PIN, SWIVEL_LS_PIN, MOTOR_CURRENT, SWIVEL_MIN, SWIVEL_MAX, SWIVEL_GEARBOX);
 StepperMotor extend = StepperMotor(EXTEND_CS_PIN, EXTEND_EN_PIN, EXTEND_LS_PIN, MOTOR_CURRENT, EXTEND_MIN, EXTEND_MAX, EXTEND_GEARBOX);
 StepperMotor lift = StepperMotor(LIFT_CS_PIN, LIFT_EN_PIN, LIFT_LS_PIN, MOTOR_CURRENT, LIFT_MIN, LIFT_MAX, LIFT_GEARBOX);
 
-void setup() {
+void setup()
+{
 	pinMode(SWIVEL_CS_PIN, OUTPUT);
 	pinMode(EXTEND_CS_PIN, OUTPUT);
 	pinMode(LIFT_CS_PIN, OUTPUT);
@@ -77,24 +78,25 @@ void setup() {
 	calibrate();
 	Serial.println("Finished stepper motor initialization.");
 
-	// BurtCan::setup();
-	// BurtCan::registerHandler(CAN_SWIVEL_ID, swivelHandler);
-	// BurtCan::registerHandler(CAN_EXTEND_ID, extendHandler);
-	// BurtCan::registerHandler(CAN_LIFT_ID, liftHandler);
-	// BurtCan::registerHandler(CAN_PRECISE_SWIVEL_ID, preciseSwivelHandler);
-	// BurtCan::registerHandler(CAN_PRECISE_EXTEND_ID, preciseExtendHandler);
-	// BurtCan::registerHandler(CAN_PRECISE_LIFT_ID, preciseLiftHandler);
-	// Serial.println("Finished CAN bus initialization.");
-	Serial.println("Skipped CAN bus initialization.");
+	BurtCan::setup();
+	BurtCan::registerHandler(CAN_SWIVEL_ID, swivelHandler);
+	BurtCan::registerHandler(CAN_EXTEND_ID, extendHandler);
+	BurtCan::registerHandler(CAN_LIFT_ID, liftHandler);
+	BurtCan::registerHandler(CAN_PRECISE_SWIVEL_ID, preciseSwivelHandler);
+	BurtCan::registerHandler(CAN_PRECISE_EXTEND_ID, preciseExtendHandler);
+	BurtCan::registerHandler(CAN_PRECISE_LIFT_ID, preciseLiftHandler);
+	Serial.println("Finished CAN bus initialization.");
+	// Serial.println("Skipped CAN bus initialization.");
 }
 
-void loop() {
+void loop()
+{
 	// while (!swivel.isFinished() || !extend.isFinished() || !lift.isFinished()) {
 	// 	swivel.fixPotentialStall();
 	// 	extend.fixPotentialStall();
 	// 	lift.fixPotentialStall();
 	// }
-	// BurtCan::update();
+	BurtCan::update();
 	// delay(1000);
 
 	// Temporary Serial Monitor interface for testing
@@ -103,13 +105,14 @@ void loop() {
 	delay(500);
 }
 
-void updatePosition(double newX, double newY, double newZ) {
+void updatePosition(double newX, double newY, double newZ)
+{
 	/* Updates the (x, y, z) coordinates of the gripper, provided it is safe. */
 	Serial.print("Trying to go to:  ");
 	Serial.print(newX);
-	Serial.print(" ");	
+	Serial.print(" ");
 	Serial.print(newY);
-	Serial.print(" ");	
+	Serial.print(" ");
 	Serial.println(newZ);
 	Coordinates newCoordinates(newX, newY, newZ);
 	Angles newAngles = ArmIK::calculateAngles(newCoordinates);
@@ -117,15 +120,18 @@ void updatePosition(double newX, double newY, double newZ) {
 	Serial.print(newAngles.theta);
 	Serial.print(", ");
 	Serial.print(newAngles.B);
-	Serial.print(", ");	
-	Serial.println(newAngles.C);	
+	Serial.print(", ");
+	Serial.println(newAngles.C);
 	swivel.moveTo(newAngles.theta);
 	extend.moveTo(newAngles.B);
 	lift.moveTo(newAngles.C);
-	gripperX = newX; gripperY = newY; gripperZ = newZ;
+	gripperX = newX;
+	gripperY = newY;
+	gripperZ = newZ;
 }
 
-void calibrate() {
+void calibrate()
+{
 	swivel.calibrate();
 	extend.calibrate();
 	lift.calibrate();
@@ -134,65 +140,78 @@ void calibrate() {
 	gripperZ = CALIBRATED_Z;
 }
 
-/* Handler functions for the controller inputs. 
- 
+/* Handler functions for the controller inputs.
+
  Many messages will send an argument from -1 to 1. However, [CanMessage]s can
  only send unsigned integers. Because of this, we need to convert to float and
  subtract 1, since the message will be from [0, 2].
 */
 
-void swivelHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void swivelHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	gripperX += ArmConstants::movementSpeed * arg;
 	updatePosition(gripperX, gripperY, gripperZ);
 }
 
-void extendHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void extendHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	gripperY += ArmConstants::movementSpeed * arg;
 	updatePosition(gripperX, gripperY, gripperZ);
 }
 
-void liftHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void liftHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	gripperZ += ArmConstants::movementSpeed * arg;
 	updatePosition(gripperX, gripperY, gripperZ);
 }
 
-void preciseSwivelHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void preciseSwivelHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	swivel.moveBy(ArmConstants::angleIncrement * arg);
 }
 
-void preciseLiftHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void preciseLiftHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	lift.moveBy(ArmConstants::angleIncrement * arg);
 }
 
-void preciseExtendHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void preciseExtendHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	extend.moveBy(ArmConstants::angleIncrement * arg);
 }
 
-void calibrateHandler(const CanMessage& message) {
+void calibrateHandler(const CanMessage &message)
+{
 	calibrate();
 }
 
-void parseSerialCommand(String input) {
+void parseSerialCommand(String input)
+{
 	Serial.println("User inputted: " + input);
-	if (input == "calibrate") calibrate();
+	if (input == "calibrate")
+		calibrate();
 
 	int delimiter = input.indexOf(" ");
-	if (delimiter == -1) return;
+	if (delimiter == -1)
+		return;
 	int delimiter2 = input.indexOf(" ", delimiter + 1);
-	if (delimiter2 != -1) {  // was given an x, y, z command
+	if (delimiter2 != -1)
+	{ // was given an x, y, z command
 		float x = input.substring(0, delimiter).toFloat();
 		float y = input.substring(delimiter + 1, delimiter2).toFloat();
 		float z = input.substring(delimiter2).toFloat();
 		Serial.print("Moving to: (");
-			Serial.print(x); Serial.print(", ");
-			Serial.print(y); Serial.print(", ");
-			Serial.print(z); 
+		Serial.print(x);
+		Serial.print(", ");
+		Serial.print(y);
+		Serial.print(", ");
+		Serial.print(z);
 		Serial.println(").");
 		gripperX += x;
 		gripperY += y;
@@ -203,17 +222,27 @@ void parseSerialCommand(String input) {
 	String command = input.substring(0, delimiter);
 	float arg = input.substring(delimiter + 1).toFloat();
 
-	if (command == "swivel") {
+	if (command == "swivel")
+	{
 		gripperX += ArmConstants::movementSpeed * arg;
 		updatePosition(gripperX, gripperY, gripperZ);
-	} else if (command == "extend") {
+	}
+	else if (command == "extend")
+	{
 		gripperY += ArmConstants::movementSpeed * arg;
 		updatePosition(gripperX, gripperY, gripperZ);
-	} else if (command == "lift") {
+	}
+	else if (command == "lift")
+	{
 		gripperZ += ArmConstants::movementSpeed * arg;
 		updatePosition(gripperX, gripperY, gripperZ);
-	} else if (command == "precise-swivel") swivel.moveBy(arg);
-	else if (command == "precise-lift") lift.moveBy(arg);
-	else if (command == "precise-extend") extend.moveBy(arg);
-	else Serial.println("Unrecognized command");
+	}
+	else if (command == "precise-swivel")
+		swivel.moveBy(arg);
+	else if (command == "precise-lift")
+		lift.moveBy(arg);
+	else if (command == "precise-extend")
+		extend.moveBy(arg);
+	else
+		Serial.println("Unrecognized command");
 }
--- Gripper/Gripper.ino
+++ Gripper/Gripper.ino
@@ -7,11 +7,9 @@
 #define ROTATE_CS_PIN 10
 #define ROTATE_LS_PIN 7
 
-
 #define LIFT_EN_PIN 34
 #define LIFT_CS_PIN 37
 #define LIFT_LS_PIN 8
-
 
 #define PINCH_EN_PIN 40
 #define PINCH_CS_PIN 36
@@ -20,14 +18,14 @@
 #define ROTATE_MIN -2 * PI
 #define ROTATE_MAX 2 * PI
 
-#define LIFT_MIN 0 
+#define LIFT_MIN 0
 #define LIFT_MAX 2.004
 
 #define PINCH_MIN 0
 #define PINCH_MAX 1.867
 
 // Limits for the joints, in terms of radians from their limit switches
-#define ROTATE_LIMIT INFINITY  // can swivel in endless circles
+#define ROTATE_LIMIT INFINITY // can swivel in endless circles
 #define PINCH_LIMIT 1.867
 #define LIFT_LIMIT 2.004
 
@@ -42,29 +40,32 @@
 #define LOW_CURRENT 1300
 #define HIGH_CURRENT 2200
 
-
 StepperMotor rotate = StepperMotor(ROTATE_CS_PIN, ROTATE_EN_PIN, ROTATE_LS_PIN, LOW_CURRENT, ROTATE_MIN, ROTATE_MAX, 99.51);
 StepperMotor pinch = StepperMotor(PINCH_CS_PIN, PINCH_EN_PIN, PINCH_LS_PIN, LOW_CURRENT, PINCH_MIN, PINCH_MAX, 199.02);
 StepperMotor lift = StepperMotor(LIFT_CS_PIN, LIFT_EN_PIN, LIFT_LS_PIN, HIGH_CURRENT, LIFT_MIN, LIFT_MAX, 29.16);
 
-void setup() { 
+void setup()
+{
 	Serial.begin(9600);
 	Serial.println("Finished stepper motor initialization.");
-	// BurtCan::setup();
-	// BurtCan::registerHandler(CAN_ROTATE_ID, rotateHandler);
-	// BurtCan::registerHandler(CAN_PINCH_ID, pinchHandler);
-	// BurtCan::registerHandler(CAN_LIFT_ID, liftHandler);
-	// BurtCan::registerHandler(CAN_PRECISE_PINCH_ID, precisePinchHandler);
-	// BurtCan::registerHandler(CAN_CALIBRATE_ID, calibrateHandler);
-	// Serial.println("Finished CAN bus initialization.");
+	BurtCan::setup();
+	BurtCan::registerHandler(CAN_ROTATE_ID, rotateHandler);
+	BurtCan::registerHandler(CAN_PINCH_ID, pinchHandler);
+	BurtCan::registerHandler(CAN_LIFT_ID, liftHandler);
+	BurtCan::registerHandler(CAN_PRECISE_PINCH_ID, precisePinchHandler);
+	BurtCan::registerHandler(CAN_CALIBRATE_ID, calibrateHandler);
+	Serial.println("Finished CAN bus initialization.");
 }
 
-void loop() {
-	while (!pinch.isFinished() || !rotate.isFinished() || !lift.isFinished()) {
+void loop()
+{
+	while (!pinch.isFinished() || !rotate.isFinished() || !lift.isFinished())
+	{
 		pinch.fixPotentialStall();
 		rotate.fixPotentialStall();
 		lift.fixPotentialStall();
 	}
+	BurtCan::update();
 	// delay(1000);
 
 	// Temporary Serial Monitor interface for testing
@@ -73,45 +74,52 @@ void loop() {
 	delay(500);
 }
 
-void calibrate() {
+void calibrate()
+{
 	pinch.calibrate();
 	rotate.calibrate();
 	lift.calibrate();
 }
 
-/* Handler functions for the controller inputs. 
- 
+/* Handler functions for the controller inputs.
+
  Many messages will send an argument from -1 to 1. However, [CanMessage]s can
  only send unsigned integers. Because of this, we need to convert to float and
  subtract 1, since the message will be from [0, 2].
 */
 
-void pinchHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void pinchHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	pinch.moveTo(pinch.angle + (ArmConstants::rotationSpeed * arg));
 }
 
-void rotateHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void rotateHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	rotate.moveTo(rotate.angle + (ArmConstants::rotationSpeed * arg));
 }
 
-void liftHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void liftHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	lift.moveTo(lift.angle + (ArmConstants::rotationSpeed * arg));
 }
 
-void precisePinchHandler(const CanMessage& message) {
-	float arg = (float) message.buf[0] - 1;
+void precisePinchHandler(const CanMessage &message)
+{
+	float arg = (float)message.buf[0] - 1;
 	pinch.moveTo(pinch.angle + (ArmConstants::angleIncrement * arg));
 }
 
-void calibrateHandler(const CanMessage& message) {
+void calibrateHandler(const CanMessage &message)
+{
 	calibrate();
 }
 
-void parseSerialCommand(String input) {
-	/* 
+void parseSerialCommand(String input)
+{
+	/*
 		pinch: [-1, 1]
 		rotate: [-1, 1]
 		lift: [-1, 1]
@@ -119,24 +127,31 @@ void parseSerialCommand(String input) {
 		calibrate;
 	*/
 
-	if (input == "calibrate") calibrate();
+	if (input == "calibrate")
+		calibrate();
 
 	int delimiter = input.indexOf(" ");
-	if (delimiter == -1) return;
+	if (delimiter == -1)
+		return;
 	String command = input.substring(0, delimiter);
 	String part2 = input.substring(delimiter + 1);
 	float arg = part2.toFloat();
-	if (arg < -1 || arg > 1) {
+	if (arg < -1 || arg > 1)
+	{
 		Serial.println("Argument must be between -1 and 1");
 		return;
 	}
 
-	if (command == "pinch") pinch.moveTo(pinch.angle + arg);
-	else if (command == "rotate") rotate.moveTo(rotate.angle + arg);
-	else if (command == "precise-pinch") {
+	if (command == "pinch")
+		pinch.moveTo(pinch.angle + arg);
+	else if (command == "rotate")
+		rotate.moveTo(rotate.angle + arg);
+	else if (command == "precise-pinch")
+	{
 		pinch.moveTo(pinch.angle + (ArmConstants::angleIncrement * arg));
-	} else {
+	}
+	else
+	{
 		Serial.println("Unknown command: " + command);
 	}
-
 }
