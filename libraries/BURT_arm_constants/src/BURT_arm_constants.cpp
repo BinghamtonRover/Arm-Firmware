ee8af4178d7418d0f9f16ce0f391fdd3f1bdc326
Levi Lesches <levilesches@gmail.com>
2022-11-30 18:02:59 -0500

    Modifications to get the Mini Arm working
    MiniArm.ino is working, controls the lower 3 motors. MiniGripper is not working, needs to be ported to work similarly to MiniArm and control the upper 3 motors. Arm/Gripper.ino are not working and need to be ported to use the new changes, primarily, the difference in the constructor.
    
    See MiniArm.ino for a list of TODOs.

--- libraries/BURT_arm_constants/src/BURT_arm_constants.cpp
+++ libraries/BURT_arm_constants/src/BURT_arm_constants.cpp
@@ -5,12 +5,21 @@
 
 #include "BURT_arm_constants.h"
 
-double ArmConstants::extendLimits[2] = { -100,  100};
-double ArmConstants::liftLimits[2] = { -2.61799, 1.8326 }; 
-double ArmConstants::swivelLimits[2] = { -2.35619, 2.44346 };
-double ArmConstants::pinchLimits[2] = {0, 1};
-double ArmConstants::gripperLiftLimits[2] = {-PI, PI};
-double ArmConstants::gripperRotateLimits[2] = {0, 2 * PI};
+// Real arm limits
+// double ArmConstants::extendLimits[2] = { -100,  100};
+// double ArmConstants::liftLimits[2] = { -2.61799, 1.8326 }; 
+// double ArmConstants::swivelLimits[2] = { -2.35619, 2.44346 };
+// double ArmConstants::pinchLimits[2] = {0, 1};
+// double ArmConstants::gripperLiftLimits[2] = {-PI, PI};
+// double ArmConstants::gripperRotateLimits[2] = {0, 2 * PI};
+
+// Demo arm limits
+double ArmConstants::extendLimits[2] = { 0, PI / 3};
+double ArmConstants::liftLimits[2] = { 0, PI / 3}; 
+double ArmConstants::swivelLimits[2] = { 0, PI / 3};
+double ArmConstants::pinchLimits[2] = { 0, PI / 3};
+double ArmConstants::gripperLiftLimits[2] = { 0, PI / 3};
+double ArmConstants::gripperRotateLimits[2] = { 0, PI / 3};
 
 // The following close bracket marks the file for Doxygen
 /*! \} */
